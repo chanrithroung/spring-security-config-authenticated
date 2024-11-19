45e1c41207326865cd35c006760fb5610f877591
chanrithroung2@gmail.com <chanrithroung2@gmail.com>
2024-11-19 10:04:24 +0700

    In this version we Bcrbpt password

--- src/main/java/com/example/spring_Security/mapper/UserMapper.java
+++ src/main/java/com/example/spring_Security/mapper/UserMapper.java
@@ -3,13 +3,16 @@ package com.example.spring_Security.mapper;
 import com.example.spring_Security.dto.requetDTO.UserRequestDTO;
 import com.example.spring_Security.dto.responseDTO.UserResponseDTO;
 import com.example.spring_Security.model.UserModel.UserModel;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class UserMapper {
     public static UserModel toEntity(UserRequestDTO requestDTO) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
         UserModel userModel = new UserModel();
         userModel.setId(requestDTO.getId());
         userModel.setUsername(requestDTO.getUsername());
-        userModel.setPassword(requestDTO.getPassword());
+        userModel.setPassword(encoder.encode(requestDTO.getPassword()));
         return userModel;
     }
 
